56c435bccb9dd4d84871a884305eb07a97af0f22
Sarang <rajurkarsarang7@gmail.com>
2023-12-30 00:52:33 +0530

    rf

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,9 +15,7 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.route('/register').post(
-    upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'coverImage', maxCount: 1 }])
-    ,registerUser);
+router.route('/register').post(upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'coverImage', maxCount: 1 }]),registerUser);
 router.route('/login').post(loginUser);   
 router.route('/logout').post(verifyJWT, logoutUser); 
 router.route('/refresh-token').post(refreshAccessToken);
