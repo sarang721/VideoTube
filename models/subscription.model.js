d4fa3359d26dbed932a0e8860e697dc879f595e0
Sarang <rajurkarsarang7@gmail.com>
2023-12-30 10:03:33 +0530

    added subscription model

---
+++ models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = mongoose.Schema({
+    subscriber:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    channel:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
+    }
+})
+
+const Subscription = mongoose.model('Subscription',subscriptionSchema);
